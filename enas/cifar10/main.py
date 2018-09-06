661db8e89ee9609f04b4001b6588c44c11939161
j-varun <vjain12@jhu.edu>
2018-09-05 23:37:49 -0400

    Bug fixes

--- enas/cifar10/main.py
+++ enas/cifar10/main.py
@@ -174,6 +174,7 @@ def get_ops(images, labels):
         stacking_reward=FLAGS.stacking_reward,
         dataset=FLAGS.dataset,
         data_base_path=FLAGS.data_base_path,
+        output_dir=FLAGS.output_dir,
         pool_distance=FLAGS.child_pool_distance,
         random_augmentation=FLAGS.random_augmentation
     )
@@ -352,11 +353,11 @@ def train():
                         log_string += "\ntr_preds={}".format(tr_preds)
                         log_string += "\ntr_label={}".format(tr_label)
                     print(log_string)
-                    if os.path.exists(FLAGS.data_base_path+"train_metrics.csv"):
+                    if os.path.exists(FLAGS.output_dir+"/train_metrics.csv"):
                         file_mode = 'a'
                     else:
-                        file_mode = 'w'
-                    with open(FLAGS.data_base_path+"train_metrics.csv", file_mode) as fp:
+                        file_mode = 'w+'
+                    with open(FLAGS.output_dir+"/train_metrics.csv", file_mode) as fp:
                         fp.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n".format(epoch, global_step, loss, tr_acc, tr_acc_2_30, tr_acc_4_60, tr_acc_8_120, tr_op, tr_angle_error, tr_cart_error, tr_mae))
 
                 if actual_step % ops["eval_every"] == 0:
@@ -408,12 +409,12 @@ def train():
                                     log_string += "\nmae={}".format(mae)
                                 # log_string += "\n g_emb = {}".format(g_emb)
                                 print(log_string)
-                                if os.path.exists(FLAGS.data_base_path+"controller_metrics.csv"):
+                                if os.path.exists(FLAGS.output_dir+"\controller_metrics.csv"):
                                     file_mode = 'a'
                                 else:
-                                    file_mode = 'w'
-                                with open(FLAGS.data_base_path+"controller_metrics.csv", file_mode ) as fp:
-                                    fp.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n".format(epoch, loss, entropy, lr, gn, val_acc, bl, reward, c_mse, cart_error, angle_error, mae))
+                                    file_mode = 'w+'
+                                with open(FLAGS.output_dir+"\controller_metrics.csv", file_mode ) as fp:
+                                    fp.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n".format(epoch, controller_step, loss, entropy, lr, gn, val_acc, bl, reward, c_mse, cart_error, angle_error, mae))
 
                         print("Here are 10 architectures")
                         for _ in range(10):
